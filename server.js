c0c0e516a75e73c5bf9d09884e6ab3b81e1b7030
christinax32 <christina.hoang@salesforce.com>
2023-06-20 21:56:47 -0700

    updating function

--- server.js
+++ server.js
@@ -59,74 +59,123 @@ db.query(
             })
         }
 
-// function addRole(){
-//     return new Promise((resolve, reject) => {
-//         db.query(
-//           'INSERT INTO role SET ?',
-//           { title: title, salary: salary, department_id: departmentId },
-//           (err, res) => {
-//             if (err) reject(err);
-//             resolve();
-//           }
-//         );
-//       });
-//     };
 
-    function addRole() {
-      inquirer.prompt([
+
+function findAllDepartments() {
+  return db
+    .promise()
+    .query("SELECT department.id, department.name FROM department;");
+}
+
+
+function addRole() {
+  findAllDepartments().then(([rows]) => {
+    let departments = rows;
+    const departmentChoices = departments.map(({ id, name }) => ({
+      name: name,
+      value: id,
+    }));
+    inquirer
+      .prompt([
         {
           name: "title",
-          message: "What is the title of the role?"
-        }
+          message: "What is the title of the role?",
+        },
+        {
+          name: "salary",
+          message: "What is the salary of the role?",
+        },
+        {
+          type: "list",
+          name: "department",
+          message: "Which department does the role belong to?:",
+          choices: departmentChoices,
+        },
       ])
-        .then(res => {
-          let name = res;
-
-          inquirer.prompt([
-            {
-              name: "salary",
-              message: "What is the salary of the role?"
-            }
-          ]) 
-          .then (res => {
-            let salary = res;
-
-            inquirer.prompt ([
-              {
-                type: 'list',
-                name: 'department',
-                message: 'Which department does the role belong to?:',
-                choices: ['Shoes', 'Women', 'Men', 'Children', 'Household'], 
-              }
-            ])
-            .then (res => {
-              let department = res;
-            } )
-          })
-          db.query( 'INSERT INTO role SET ?',
-          { title: VALUES('${res.title}'), salary: VALUES('${res.salary}'), department_id: VALUES('${res.department}') }, function (err, results) {
+      .then((res) => {
+        console.log(res);
+        db.query(
+          "INSERT INTO role SET ?",
+          {
+            title: res.title,
+            salary: res.salary,
+            department_id: res.department,
+          },
+          function (err, results) {
             if (err) {
               console.log(err);
             }
-            console.log(`added ${res.title} to the database`);
-            handleOptions()
-          });
-        })
-    }
-
-
-function addEmployee(){
-    return new Promise((resolve, reject) => {
-        db.query(
-          'INSERT INTO employee SET ?',
-          { first_name: firstName, last_name: lastName, role_id: roleId, manager_id: managerId },
-          (err, res) => {
-            if (err) reject(err);
-            resolve();
+            console.log('added ${res.title} to the database');
+            handleOptions();
           }
         );
       });
-    };
+  });
+}
+      
+function findAllEmployees() {
+  return db
+    .promise()
+    .query("SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id  FROM employee;");
+}
+
+
+function addEmployee(){
+  findAllEmployees().then(([rows]) => {
+    let employees = rows;
+    const employeeChoices = employees.map(({ id, first_name, last_name, role_id, manager_id  }) => ({
+      firstname: first_name,
+      value: id,
+      lastname: last_name,
+      roleid: role_id,
+      managerid: manager_id
+    }));
+  inquirer
+  .prompt([
+    {
+      name: "first",
+      message: "What is the employee's first name?",
+    },
+    {
+      name: "last",
+      message: "What is the employee's last name?",
+    },
+    {
+      type: "list",
+      name: "role",
+      message: "What is the employee's role",
+      choices: employeeChoices,
+    },
+    {
+      type: "list",
+      name: "manager",
+      message: "Who is the employee's manager?",
+      choices: 'Michael Jackson',
+    }
+  ])
+  .then((res) => {
+    console.log(res);
+    db.query(
+      "INSERT INTO employee SET ?",
+      {
+        first: res.first_name,
+        last: res.last_name,
+        role: res.role,
+        manager: res.manager
+
+      },
+      function (err, results) {
+        if (err) {
+          console.log(err);
+        }
+        console.log('added ${res.first_name} ${res.last_name}  to the database');
+        handleOptions();
+      }
+    );
+  });
+});
+}
+
 function updateEmployeeRole(){
     return new Promise((resolve, reject) => {
         db.query(
