ecec9674608fbebb64c79e3685a833899e62d5db
christinax32 <christina.hoang@salesforce.com>
2023-06-21 18:11:07 -0700

    fixing seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,12 +6,12 @@ VALUES ("Shoes"),
        ("Household");
 
 INSERT INTO role (title, salary,department_id)
-VALUES ("manager", "100000", 1),
-       ("intern", "30000", 2),
-       ("employee", "50000", 3);
+VALUES ("manager", 100000, 1),
+       ("intern", 30000, 2),
+       ("employee", 50000, 3);
 
 INSERT INTO employee (first_name, last_name,role_id, manager_id)
 VALUES ("Christina", "Hoang", 1, NULL),
        ("Michael", "Jackson", 2, 2),
        ("Billie", "Eilish", 3, 2),
-       ("John", "Smith", 4, 2)
+       ("John", "Smith", 2, 2)
